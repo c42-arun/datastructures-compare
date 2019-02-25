ce9d73eb34b75a8d6c43933ebc090c2318c127dc
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2019-02-25 10:50:55 +0000

    with only List data structure (dynamic array)

---
+++ M3 - Before/StockTrader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Module3Before
+{
+    class StockTrader : IStockTrader
+    {
+        List<InvestmentQuery> stocksToTrade = new List<InvestmentQuery>();
+
+        public void EnqueueStockForTrading(InvestmentQuery query)
+        {
+            stocksToTrade.Add(query);
+        }
+
+        public void HandledTradings()
+        {
+            Console.Write(" [{0} stocks] ", stocksToTrade.Count);
+            while(stocksToTrade.Count > 0)
+            {
+                var query = stocksToTrade[0];
+                // As this is simulation of a real service that consumes queries as they arrive,
+                // remember to remove the query from the list when processed: 
+                stocksToTrade.RemoveAt(0);
+                
+                // Simulate stock trade:
+//                Thread.Sleep(100);
+            }
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,5 @@
+https://app.pluralsight.com/library/courses/algorithmics-introduction
+
+Querying... Done in 0.23 s
+Analyzing queries... Done in 123.35 s
+Handling tradings...  [9634 stocks] Done in 0.02 s
