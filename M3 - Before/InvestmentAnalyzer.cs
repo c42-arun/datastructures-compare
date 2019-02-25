d5d2111ecfa25cc3054801e8695194891a07d7c1
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2019-02-25 11:46:15 +0000

    Using Dictionary instead of List for cache lookup:(11 seconds -> 0.66 seconds)

--- M3 - Before/InvestmentAnalyzer.cs
+++ M3 - Before/InvestmentAnalyzer.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 
 namespace Module3Before
 {
-    class RatingCacheElement
-    {
-        public string StockID { get; set; }
-        public int Rating { get; set; }
-    }
+    //class RatingCacheElement
+    //{
+    //    public string StockID { get; set; }
+    //    public int Rating { get; set; }
+    //}
 
     class InvestmentAnalyzer
     {
         IStockTrader stockTrader;
         C5.IntervalHeap<InvestmentQuery> queries = new C5.IntervalHeap<InvestmentQuery>();
-        List<RatingCacheElement> stock2rating = new List<RatingCacheElement>();
+        Dictionary<string, int> stock2rating = new Dictionary<string, int>();
         Random random = new Random(29);
 
         public InvestmentAnalyzer(IStockTrader stockTrader)
@@ -33,15 +33,13 @@ namespace Module3Before
                 int rating;
                 var query = queries.DeleteMin(); // Get first-priority queries first
 
-                var cacheElement = stock2rating.Find(x => x.StockID == query.StockID);
-                if (cacheElement != null)
-                    rating = cacheElement.Rating;
+                //var cacheElement = stock2rating.Find(x => x.StockID == query.StockID);
+                if (stock2rating.ContainsKey(query.StockID))
+                    rating = stock2rating[query.StockID];
                 else
                 {
                     rating = CalculateRating(query.StockID);
-                    stock2rating.Add(
-                        new RatingCacheElement { StockID = query.StockID, Rating = rating }
-                    );
+                    stock2rating.Add(query.StockID, rating);
                 }
 
                 if (rating > 80) // Let's say that a rating of 80 triggers a stock trade
--- readme.txt
+++ readme.txt
@@ -1,8 +1,25 @@
 https://app.pluralsight.com/library/courses/algorithmics-introduction
 
-Using C5.IntervalHeap instead of List for PriorityQueue:
-123 seconds -> 11 seconds
+All List as data structures: (124 seconds)
+Querying... Done in 0.23 s
+Analyzing queries... Done in 123.35 s
+Handling tradings...  [9634 stocks] Done in 0.02 s
 
+Using C5.IntervalHeap instead of List for PriorityQueue: (123 seconds -> 11 seconds)
 Querying... Done in 0.47 s
 Analyzing queries... Done in 11.12 s
 Handling tradings...  [9716 stocks] Done in 0 s
+
+
+Using Dictionary instead of List for cache lookup:(11 seconds -> 0.66 seconds)
+Querying... Done in 0.22 s
+Analyzing queries... Done in 0.66 s
+Handling tradings...  [9625 stocks] Done in 0.01 s
+
+---------------------
+Using List as Queue:
+Trading 200K stocks [200000 stocks] Done in 14.06
+
+Using LinkedList as Queue:
+Trading 200K stocks [200000 stocks] Done in 0.06
+---------------------
